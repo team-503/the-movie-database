5d6c2613fc729e1aae82483d6404744337cc2c83
Oleshkooo <olehchoma@gmail.com>
2024-11-16 18:19:10 +0200

    chore: configure prettier formatter

--- packages/config-eslint/prettier-decorators.js
+++
@@ -1,8 +0,0 @@
-/** @typedef  {import("prettier").Config} PrettierConfig */
-
-/** @type { PrettierConfig | SortImportsConfig } */
-const config = {
-    importOrderParserPlugins: ['typescript', 'decorators-legacy'],
-}
-
-module.exports = config
--- packages/dto/.prettierrc.js
+++
@@ -1,5 +0,0 @@
-/** @type {import("prettier").Config} */
-module.exports = {
-    ...require('@repo/eslint-config/prettier-base'),
-    ...require('@repo/eslint-config/prettier-decorators'),
-}
