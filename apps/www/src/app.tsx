94fb92d1ab2c7ea74a8197f18976c4659d2c20d4
Oleshkooo <olehchoma@gmail.com>
2024-11-16 18:18:54 +0200

    chore: create initial 'www' app

---
+++ apps/www/src/app.tsx
@@ -0,0 +1,7 @@
+import { memo } from 'react'
+
+type AppProps = unknown
+export const App: React.FC<AppProps> = memo(() => {
+    return <></>
+})
+App.displayName = 'App'
--- packages/pkg-helpers/src/utils/index.ts
+++ packages/pkg-helpers/src/utils/index.ts
@@ -1,1 +1,2 @@
 export { assert } from 'assert-ts'
+export * from './cn'
