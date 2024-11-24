eb0c5c984f169fb5e4b181f02cdcc0f9534d3ac4
Oleshkooo <olehchoma@gmail.com>
2024-11-24 22:27:01 +0200

    chore: refactor helpers package

--- packages/pkg-helpers/src/utils/index.ts
+++ packages/pkg-helpers/src/utils/index.ts
@@ -1,2 +1,1 @@
 export { assert } from 'assert-ts'
-export * from './cn'
--- packages/pkg-helpers/tsup.config.ts
+++ packages/pkg-helpers/tsup.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig, type Options } from 'tsup'
 
 export default defineConfig((options: Options) => ({
-    entryPoints: ['src/index.ts'],
-    clean: true,
     dts: true,
+    clean: true,
     format: ['cjs'],
+    entryPoints: ['src/index.ts'],
     ...options,
 }))
