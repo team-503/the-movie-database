2890b328e1d682747e38b62ea03fc9de4bfcd8fe
Oleshkooo <olehchoma@gmail.com>
2024-12-13 11:12:40 +0200

    feat(www): add similar movies section

--- apps/www/src/modules/movie/components/movie-cards-list-snap-scroll.tsx
+++ apps/www/src/modules/movie/components/movie-cards-list-snap-scroll.tsx
@@ -7,12 +7,12 @@ import { Link, To } from 'react-router-dom'
 type MovieCardsListSnapScrollProps = {
     movies: MovieEntity[]
     title: string
-    to: To
+    to?: To
 }
 export const MovieCardsListSnapScroll: React.FC<MovieCardsListSnapScrollProps> = memo(({ movies, title, to }) => {
     return (
         <div className="space-y-3">
-            <Link to={to} className="hover:text-muted-foreground transition-color flex w-fit duration-100">
+            <Link to={to ?? ''} className="hover:text-muted-foreground transition-color flex w-fit duration-100">
                 <p className="border-l-4 border-yellow-500 pl-3 text-2xl font-semibold">{title}</p>
                 <ChevronRight className="~w-8/9 ~h-8/9" />
             </Link>
--- apps/www/src/pages/movie-id/movie-id-page.tsx
+++ apps/www/src/pages/movie-id/movie-id-page.tsx
@@ -1,6 +1,7 @@
 import { memo, useMemo } from 'react'
-import { useGetMovieDetailsQuery, useGetMovieImagesQuery } from '@/__generated__/graphql'
+import { useGetMovieDetailsQuery, useGetMovieImagesQuery, useGetSimilarMoviesQuery } from '@/__generated__/graphql'
 import { URLs } from '@/common/urls'
+import { MovieCardsListSnapScroll } from '@/modules/movie/components/movie-cards-list-snap-scroll'
 import dayjs from 'dayjs'
 import { Star } from 'lucide-react'
 import { Navigate, useParams } from 'react-router-dom'
@@ -11,10 +12,11 @@ export const MovieIdPage: React.FC<MovieIdPageProps> = memo(() => {
     const { id } = useParams<'id'>()
     const { data: getMovieDetailsData, loading } = useGetMovieDetailsQuery({ variables: { movieId: Number(id) } })
     const { data: getMovieImagesData } = useGetMovieImagesQuery({ variables: { movieId: Number(id) } })
+    const { data: similarMoviesData } = useGetSimilarMoviesQuery({ variables: { movieId: Number(id) } })
 
     const movie = useMemo(() => getMovieDetailsData?.getMovieDetails, [getMovieDetailsData?.getMovieDetails])
     const imagePaths = useMemo(
-        () => [...new Set(getMovieImagesData?.getMovieImages.backdrops.map(({ file_path }) => file_path))],
+        () => [...new Set(getMovieImagesData?.getMovieImages.backdrops.map(({ file_path }) => file_path))].slice(0, 20),
         [getMovieImagesData?.getMovieImages?.backdrops]
     )
 
@@ -26,60 +28,63 @@ export const MovieIdPage: React.FC<MovieIdPageProps> = memo(() => {
     }
 
     return (
-        <div className="flex">
-            {/* left */}
-            <div className="flex-1 p-10">
-                <FullscreenImagePreview src={URLs.tmdb.getImageURL(movie.poster_path)} className="rounded-3xl" />
-            </div>
+        <>
+            <div className="flex">
+                {/* left */}
+                <div className="flex-1 p-10">
+                    <FullscreenImagePreview src={URLs.tmdb.getImageURL(movie.poster_path)} className="rounded-3xl" />
+                </div>
 
-            {/* right */}
-            <div className="flex-[2] space-y-8 p-10">
-                {/* common */}
-                <section className="flex items-center justify-between">
-                    <div className="space-y-4">
-                        {/* title */}
-                        <p className="text-foreground text-6xl font-medium">{movie.title}</p>
-                        {/* tagline */}
-                        {movie.tagline && <p className="text-4xl font-thin">{movie.tagline}</p>}
-                        {/* year, genres */}
-                        <p className="text-muted-foreground divide-muted-foreground divide-x-2 *:px-4">
-                            <span className="first:pl-0">{dayjs(movie.release_date).get('year')}</span>
-                            {movie.genres.map(({ name }) => (
-                                <span key={name} className="last:pr-0">
-                                    {name}
-                                </span>
+                {/* right */}
+                <div className="flex-[2] space-y-8 p-10">
+                    {/* common */}
+                    <section className="flex items-center justify-between">
+                        <div className="space-y-4">
+                            {/* title */}
+                            <p className="text-foreground text-6xl font-medium">{movie.title}</p>
+                            {/* tagline */}
+                            {movie.tagline && <p className="text-4xl font-thin">{movie.tagline}</p>}
+                            {/* year, genres */}
+                            <p className="text-muted-foreground divide-muted-foreground divide-x-2 *:px-4">
+                                <span className="first:pl-0">{dayjs(movie.release_date).get('year')}</span>
+                                {movie.genres.map(({ name }) => (
+                                    <span key={name} className="last:pr-0">
+                                        {name}
+                                    </span>
+                                ))}
+                            </p>
+                        </div>
+                        <div className="flex gap-1.5">
+                            {/* rating */}
+                            <p className="text-foreground text-4xl font-normal">{movie.vote_average.toFixed(1)}</p>
+                            <Star className="h-9 w-9 fill-yellow-500 stroke-yellow-500" />
+                        </div>
+                    </section>
+
+                    {/* overview */}
+                    <section className="space-y-4">
+                        <p className="border-l-4 border-yellow-500 pl-3 text-3xl font-semibold">Overview</p>
+                        <p className="text-balance text-base font-light">{movie.overview}</p>
+                    </section>
+
+                    {/* images */}
+
+                    <section className="space-y-4">
+                        <p className="border-l-4 border-yellow-500 pl-3 text-3xl font-semibold">Images</p>
+                        <div className="grid grid-cols-2 gap-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 2xl:grid-cols-8">
+                            {imagePaths.map(imagePath => (
+                                <FullscreenImagePreview
+                                    key={imagePath}
+                                    src={URLs.tmdb.getImageURL(imagePath)}
+                                    className="shrink-0 snap-start rounded-lg"
+                                />
                             ))}
-                        </p>
-                    </div>
-                    <div className="flex gap-1.5">
-                        {/* rating */}
-                        <p className="text-foreground text-4xl font-normal">{movie.vote_average.toFixed(1)}</p>
-                        <Star className="h-9 w-9 fill-yellow-500 stroke-yellow-500" />
-                    </div>
-                </section>
-
-                {/* overview */}
-                <section className="space-y-4">
-                    <p className="border-l-4 border-yellow-500 pl-3 text-3xl font-semibold">Overview</p>
-                    <p className="text-balance text-base font-light">{movie.overview}</p>
-                </section>
-
-                {/* images */}
-
-                <section className="space-y-4">
-                    <p className="border-l-4 border-yellow-500 pl-3 text-3xl font-semibold">Images</p>
-                    <div className="grid grid-cols-2 gap-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 2xl:grid-cols-8">
-                        {imagePaths.map(imagePath => (
-                            <FullscreenImagePreview
-                                key={imagePath}
-                                src={URLs.tmdb.getImageURL(imagePath)}
-                                className="shrink-0 snap-start rounded-lg"
-                            />
-                        ))}
-                    </div>
-                </section>
+                        </div>
+                    </section>
+                </div>
             </div>
-        </div>
+            <MovieCardsListSnapScroll movies={similarMoviesData?.getSimilarMovies.results ?? []} title="Similar movies" />
+        </>
     )
 })
 MovieIdPage.displayName = 'MovieIdPage'
